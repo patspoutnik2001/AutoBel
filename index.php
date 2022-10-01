22a785ea51db60e01ae18109ce569bb1005b009c
Patryk Krasucki <krasuckipatryk2001@gmail.com>
2022-10-01 13:13:54 +0200

    improved index

--- index.php
+++ index.php
@@ -9,16 +9,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>AutoBel - Achat et Vente voitures d'occasion</title>
         <link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">     
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     </head>
-    <body class="body">
-        <div class="navbar">
-            <div class="container">
-                <a class="logo" href="index.php"><img src="images/logoAutobel1.png" ></a>
-                <img id="mobile-cta" class="mobile-menu" src="images/menu.svg" alt="Open navigation">
+    <body class="body" >
+        <div class="navbar" >
+            <div class="container" >
+                <a class="logo"  href="index.php"><img src="images/logoAutobel1.png" >
+                    <img id="mobile-cta" class="mobile-menu" src="images/menu.svg" alt="Open navigation">
+                </a>
                 <nav>
-                    <img id="mobile-exit" class="mobile-menu-exit" src="images/exit.svg" alt="">
                     <ul class="primary-nav">
-                        <li class="current"><a href="index.php">Acceil</a></li>
+                        <li class="current"><a href="index.php">Accueil</a></li>
                         <li><a href="toutAnnonces.php">Annonces</a></li>
                         <?php
                             if(isset($_SESSION['sesNom']))
@@ -70,6 +72,9 @@
                 nav.classList.remove('menu-btn');
             })
         </script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     </body>
 </html>
